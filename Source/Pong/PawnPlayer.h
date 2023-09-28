daca0858977951f74937e5a84d323ddf6f2c1519
Chernysh_Vova <chernyshvova@gmail.com>
2023-09-28 22:02:55 +0300

    Initial commit

---
+++ Source/Pong/PawnPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "PawnPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PONG_API UPawnPlayer : public UFloatingPawnMovement
+{
+	GENERATED_BODY()
+	
+};
